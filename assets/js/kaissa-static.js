751395527271933eb26ed54630ebdad39d0f5635
Pierre Chaussalet <pierre.chaussalet@mappy.com>
2012-12-07 07:02:57 +0100

    Add js and content

---
+++ assets/js/kaissa-static.js
@@ -0,0 +1,40 @@
+var PAGES = ['accueil', 'formules', 'trouver', 'contact'];
+var LINKS = $('#links').children('li:not(li.nav-header)');
+
+var displayPage = function(page, target) {
+  target = $(target);
+  for (var i=0; i<LINKS.length; i++) {
+    $(LINKS[i]).removeClass("active");
+  }
+  for (var i=0; i<PAGES.length; i++) {
+    var link = $('#'+PAGES[i])
+    if (PAGES[i] == page) {
+      link.css('display', 'block');
+      target.parent().addClass("active");
+    } else {
+      link.css('display', 'none');
+    }
+  }
+}
+
+var loadMap = function() {
+  var canvas = $('#map_canvas');
+  var container = canvas.parent();
+  canvas.css('width', container.css('width'));
+
+  var center = new google.maps.LatLng(48.9092, 2.389);
+  var mapOptions = {
+    center: center,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  var markerOptions = {
+    position: center,
+    map: map
+  }
+  var marker = new google.maps.Marker(markerOptions);
+
+  google.maps.event.addListener(map, 'click', function() { console.log(map.getBounds()); });
+}
